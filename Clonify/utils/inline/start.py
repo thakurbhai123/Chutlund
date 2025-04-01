a11f8548bc08ee3b09a470f56276922c0c1baedd
ησᴛ ᴄσᴅєʀ <shamansh3137@gmail.com>
2025-04-01 11:26:39 +0530

    Update start.py

--- Clonify/utils/inline/start.py
+++ Clonify/utils/inline/start.py
@@ -34,7 +34,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.SOURCE)
+            InlineKeyboardButton(text=_["S_B_12"], callback_data="gib_source")
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
     ]
