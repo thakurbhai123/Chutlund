a51ccf9ed09790ecca93c1b898055e48a7947a5b
BADNAM XD <yadavxd746@gmail.com>
2025-07-07 06:25:54 +0530

    Update thumbnails.py

--- Clonify/utils/thumbnails.py
+++ Clonify/utils/thumbnails.py
@@ -74,7 +74,7 @@ async def get_thumb(videoid):
         arial = ImageFont.truetype("Clonify/assets/font2.ttf", 30)
         font = ImageFont.truetype("Clonify/assets/font.ttf", 30)
         text_size = draw.textsize("TEAM KRITI BOTS    ", font=font)
-        draw.text((1280 - text_size[0] - 10, 10), "TEAM KRITI BOTS    ", fill="white", font=font)
+        draw.text((1280 - text_size[0] - 10, 10), "TEAM KRITI BOTS    ", fill="yellow", font=font)
         draw.text(
             (55, 560),
             f"{channel} | {views[:23]}",
