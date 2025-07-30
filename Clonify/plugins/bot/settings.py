26b2d91b7370ce82dba3a73f382c70695a5fc8ff
thakurbhai123 <thakurbhai80938@gmail.com>
2025-07-30 15:17:19 +0530

    Update settings.py

--- Clonify/plugins/bot/settings.py
+++ Clonify/plugins/bot/settings.py
@@ -100,7 +100,7 @@ async def gib_repo(client, CallbackQuery, _):
         ),
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton(text="ᯓ𓆰꯭𝅃꯭🔥꯭𝝙꯭𝝸𝞀꯭𝗵꯭𝝰 ꯭𝝱꯭𝝰꯭𝝱𝝲꯭⟶᯦꯭💸꯭〞", user_id=config.OWNER_ID)],
+                [InlineKeyboardButton(text="ᯓ𓆰꯭𝅃꯭🔥꯭𝗥𝗜𝗦𝗛𝗔𝗡𝗧⟶᯦꯭💸꯭〞", user_id=7649370492)],
                 [InlineKeyboardButton(text="⌯ ᴄʟσsє ⌯", callback_data="close")]
             ]
         )
